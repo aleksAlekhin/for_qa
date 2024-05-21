e55b90cf7487f6e799d81a87b94d8a83667da779
aleksAlekhin <alekhinaleksandr4@gmail.com>
2024-05-21 20:52:57 +0200

    objects 4.1

--- index.js
+++ index.js
@@ -262,4 +262,69 @@ ask(
     () => alert("Вы отменили выполнение.")
   );
 
+//   Objects 4.1
+
+// Напишите код, выполнив задание из каждого пункта отдельной строкой:
+
+// Создайте пустой объект user.
+// Добавьте свойство name со значением John.
+// Добавьте свойство surname со значением Smith.
+// Измените значение свойства name на Pete.
+// Удалите свойство name из объекта.
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+// Проверка на пустоту
+function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
+//   Можно ли изменить объект, объявленный с помощью const? Как вы думаете? 
+const user = {
+    name: "John"
+  };
   
+  // это будет работать? ДА это сработает
+  user.name = "Pete";
+
+//   Сумма свойств объекта
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  
+  function sumSalaries(obj) {
+    let sum = 0;
+    for (let key of Object.keys(obj)) {
+      sum += obj[key];
+    }
+    return sum;
+  }
+  
+  let sum = sumSalaries(salaries);
+  console.log(sum)
+
+// Умножаем все числовые свойства на 2
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(obj) {
+    for (let key of Object.keys(obj)) {
+      if (typeof obj[key] === 'number') {
+        obj[key] *= 2;
+      }
+    }
+  };
+
+  multiplyNumeric(menu);
+  
+  console.log(menu);
